e9dd75d0f1ae226fa52150402ff29eefc6b995f3
Matthias Fuchs <matthias.fuchs@devtops.gmbh>
2020-06-18 22:03:46 +0200

    azure connect

--- src/daos/Heat/HeatDao.ts
+++ src/daos/Heat/HeatDao.ts
@@ -1,14 +1,40 @@
+import '../../LoadEnv';
+
 import { IHeat } from '../../entities/Heat';
 import { Client, types } from 'cassandra-driver';
 
 import Logger from '../../shared/Logger';
 import logger from '../../shared/Logger';
 
-const client = new Client({
-    contactPoints: ['127.0.0.1'],
-    localDataCenter: 'datacenter1',
-    keyspace: 'colorado'
-});
+const CONTACTPOINT = process.env.CONTACTPOINT !== undefined ? process.env.CONTACTPOINT : 'localhost'
+const CASSANDRA_USER = process.env.CASSANDRA_USER !== undefined ? process.env.CASSANDRA_USER : 'localhost'
+const CASSANDRA_PASSWORD = process.env.CASSANDRA_PASSWORD !== undefined ? process.env.CASSANDRA_PASSWORD : 'localhost'
+
+const useroptions = {
+    username: CASSANDRA_USER,
+    password: CASSANDRA_PASSWORD
+}
+const connectoptions = {
+    contactPoints: [CONTACTPOINT],
+    localDataCenter: process.env.LOCALDATACENTER,
+    keyspace: process.env.KEYSPACE,
+    credentials: useroptions
+}
+
+const sslconnect = {
+    contactPoints: [CONTACTPOINT],
+    localDataCenter: process.env.LOCALDATACENTER,
+    keyspace: process.env.KEYSPACE,
+    credentials: useroptions,
+    sslOptions: {
+        rejectUnauthorized: false
+    }
+}
+
+const conn = process.env.SSLCONNECT === 'true' ? sslconnect  : connectoptions
+logger.info(JSON.stringify(conn))
+
+const client = new Client(conn);
 
 const wkid = 1;
 
